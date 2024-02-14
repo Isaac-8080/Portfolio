d378e27b72cd8b9a607e52b64937a06011b0bb8b
Isaac-8080 <73018818+Isaac-8080@users.noreply.github.com>
2024-02-14 04:48:18 +0000

    Add files via upload
    index.html, style.css and script.js updated

--- script/script.js
+++ script/script.js
@@ -1,21 +1,17 @@
 // A function to display and hide the navigation menu of small screens when clicked on
 
-function displayNone() {
-  document.querySelector("#toggle").style.display = 'none';
-}
-displayNone();
-
-
 function smallScreensNav() {
 
   let smallScreensNav = document.querySelector("#toggle");
 
-  smallScreensNav.style.display = 'hidden';
+  if (smallScreensNav.style.display === "block") {
 
-  if (smallScreensNav.style.display === "none") {
-    smallScreensNav.style.display = "block";
-  } else{
     smallScreensNav.style.display = "none";
+
+  } else{
+
+    smallScreensNav.style.display = "block";
+
   }
 
 }
